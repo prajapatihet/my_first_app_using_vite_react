18a946bc629c25346dbce9bdbe81cf4bbd53b732
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-16 14:46:10 +0530

    function and class component

---
+++ src/Components/ClassComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2>ClassComponent</h2>
+            </div>
+        )
+    }
+}
---
+++ src/Components/FunctionComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function FunctionComponent() {
+  return (
+    <>
+      <div>
+        <h1>
+          FunctionComponent 1
+        </h1>
+      </div>
+      <div>
+        <h1>
+          FunctionComponent 2
+        </h1>
+      </div>
+    </>
+  )
+}
